5724f1cc98753e8e36440d4e3070f9db381525e7
lex <luohai2233@163.com>
2022-05-06 22:44:43 +0800

    update

--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -2,136 +2,99 @@
     <div class="">
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-lx-copy"></i> tab选项卡</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-lx-copy"></i> 配置选项卡</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="container">
-            <el-tabs v-model="message">
-                <el-tab-pane :label="`未读消息(${state.unread.length})`" name="first">
-                    <el-table :data="state.unread" :show-header="false" style="width: 100%">
-                        <el-table-column>
-                            <template #default="scope">
-                                <span class="message-title">{{scope.row.title}}</span>
-                            </template>
-                        </el-table-column>
-                        <el-table-column prop="date" width="180"></el-table-column>
-                        <el-table-column width="120">
-                            <template #default="scope">
-                                <el-button size="small" @click="handleRead(scope.$index)">标为已读</el-button>
-                            </template>
-                        </el-table-column>
-                    </el-table>
-                    <div class="handle-row">
-                        <el-button type="primary">全部标为已读</el-button>
-                    </div>
-                </el-tab-pane>
-                <el-tab-pane :label="`已读消息(${state.read.length})`" name="second">
-                    <template v-if="message === 'second'">
-                        <el-table :data="state.read" :show-header="false" style="width: 100%">
-                            <el-table-column>
-                                <template #default="scope">
-                                    <span class="message-title">{{scope.row.title}}</span>
-                                </template>
-                            </el-table-column>
-                            <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
-                                <template #default="scope">
-                                    <el-button type="danger" @click="handleDel(scope.$index)">删除</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                        <div class="handle-row">
-                            <el-button type="danger">删除全部</el-button>
-                        </div>
-                    </template>
-                </el-tab-pane>
-                <el-tab-pane :label="`回收站(${state.recycle.length})`" name="third">
-                    <template v-if="message === 'third'">
-                        <el-table :data="state.recycle" :show-header="false" style="width: 100%">
-                            <el-table-column>
-                                <template #default="scope">
-                                    <span class="message-title">{{scope.row.title}}</span>
-                                </template>
-                            </el-table-column>
-                            <el-table-column prop="date" width="150"></el-table-column>
-                            <el-table-column width="120">
-                                <template #default="scope">
-                                    <el-button @click="handleRestore(scope.$index)">还原</el-button>
-                                </template>
-                            </el-table-column>
-                        </el-table>
-                        <div class="handle-row">
-                            <el-button type="danger">清空回收站</el-button>
-                        </div>
-                    </template>
-                </el-tab-pane>
-            </el-tabs>
+            <el-button slot="reference">重新加载配置</el-button>
+            <div class="table_area">
+                <el-table
+                    :data="tableData"
+                    stripe
+                    style="width: 100%">
+                    <el-table-column
+                        prop="date"
+                        label="日期"
+                        width="180">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="姓名"
+                        width="180">
+                    </el-table-column>
+                    <el-table-column
+                        prop="address"
+                        label="地址">
+                    </el-table-column>
+                </el-table>
+            </div>
+            <el-divider></el-divider>
+            
+            <div class="input_area">
+                <el-collapse v-model="activeNames">
+                    <el-collapse-item title="新增配置" name="1">
+                        <el-form :inline="false" :model="config" class="form-inline" label-width="200px" label-position="left">
+                        <el-form-item label="OpenStack版本">
+                            <el-select v-model="config.name" placeholder="pike" class="label_input">
+                            <el-option label="pike" value="pike"></el-option>
+                            <el-option label="train" value="train"></el-option>
+                            </el-select>
+                        </el-form-item>
+                        <el-form-item label="url(只填写ip即可)">
+                            <el-input v-model="config.url" placeholder="99.0.85.123" class="label_input"></el-input>
+                        </el-form-item>
+                        <el-form-item label="登录云平台的用户名(admin)">
+                            <el-input v-model="config.openstackUser" placeholder="admin" class="label_input"></el-input>
+                        </el-form-item>
+                        <el-form-item label="登录云平台密码(123456)">
+                            <el-input v-model="config.openstackPassword" placeholder="123456" class="label_input"></el-input>
+                        </el-form-item>
+                        <el-form-item label="云平台租户(admin)">
+                            <el-input v-model="config.openstackScope" placeholder="admin" class="label_input"></el-input>
+                        </el-form-item>
+                        <el-form-item label="后台用户名(root)">
+                            <el-input v-model="config.controllerUser" placeholder="root" class="label_input"></el-input>
+                        </el-form-item>
+                        <el-form-item label="后台密码(123456)">
+                            <el-input v-model="config.controllerPassword" placeholder="123456" class="label_input"></el-input>
+                        </el-form-item>
+                        <el-form-item>
+                            <el-button type="primary" @click="onSubmit">新增</el-button>
+                        </el-form-item>
+                    </el-form>
+                    </el-collapse-item>
+                </el-collapse>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { ref, reactive } from "vue";
 export default {
     name: "tabs",
-    setup() {
-        const message = ref("first");
-        const state = reactive({
-            unread: [
-                {
-                    date: "2018-04-19 20:00:00",
-                    title: "【系统通知】该系统将于今晚凌晨2点到5点进行升级维护",
-                },
-                {
-                    date: "2018-04-19 21:00:00",
-                    title: "今晚12点整发大红包，先到先得",
-                },
-            ],
-            read: [
-                {
-                    date: "2018-04-19 20:00:00",
-                    title: "【系统通知】该系统将于今晚凌晨2点到5点进行升级维护",
-                },
-            ],
-            recycle: [
-                {
-                    date: "2018-04-19 20:00:00",
-                    title: "【系统通知】该系统将于今晚凌晨2点到5点进行升级维护",
-                },
-            ],
-        });
-
-        const handleRead = (index) => {
-            const item = state.unread.splice(index, 1);
-            console.log(item);
-            state.read = item.concat(state.read);
-        };
-        const handleDel = (index) => {
-            const item = state.read.splice(index, 1);
-            state.recycle = item.concat(state.recycle);
-        };
-        const handleRestore = (index) => {
-            const item = state.recycle.splice(index, 1);
-            state.read = item.concat(state.read);
-        };
-
-        return {
-            message,
-            state,
-            handleRead,
-            handleDel,
-            handleRestore,
-        };
+    data() {
+      return {
+        config: {
+            name: '',
+            url: '',
+            openstackUser: '',
+            openstackPassword: '',
+            openstackScope: '',
+            controllerUser: '',
+            controllerPassword: '',
+        }
+      }
     },
 };
 </script>
 
 <style>
-.message-title {
-    cursor: pointer;
+.container{
+    height: 100%vh !important;
 }
-.handle-row {
-    margin-top: 30px;
+.label_input{
+    width: 200px;
 }
+
 </style>
 
